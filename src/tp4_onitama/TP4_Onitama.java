be0de7d25b9a2c995797db2707d3caabffc66360
ASUS <ASUS@192.168.115.145>
2021-11-30 16:45:50 +0100

    test

--- src/tp4_onitama/TP4_Onitama.java
+++ src/tp4_onitama/TP4_Onitama.java
@@ -4,7 +4,7 @@ package tp4_onitama;
 public class TP4_Onitama {
 
     public static void main(String[] args) {
-        System.out.println("BISOUUUUUUUUUUUU");
+        System.out.println("TEST");
     }
     
 }
