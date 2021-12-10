0434344c7afb292b11e8d4aa58f20bb4e20442aa
thvel <thvel@192.168.115.164>
2021-12-10 16:46:40 +0100

    Onitama sceance 2 / Class Case

--- src/tp4_onitama/TP4_Onitama.java
+++ src/tp4_onitama/TP4_Onitama.java
@@ -6,13 +6,16 @@ public class TP4_Onitama {
     public static void main(String[] args) {
 
         Pion piontest = new Pion("rouge", true) ;
+        Case casetest = new Case();
     
         System.out.println("le pion est un " + piontest.typePion() + " de couleur " + piontest.lireCouleur());
         
-        
-        
-        
-    
+        casetest.PoserPion(piontest);
+        casetest.PoserPion(piontest);
+        casetest.EnleverPion(piontest);
+        casetest.EnleverPion(piontest);
+        casetest.PoserPion(piontest);
+        System.out.println(casetest.LirecouleurJetonCase());
         
         
     
