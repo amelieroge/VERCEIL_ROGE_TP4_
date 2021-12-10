584ccd7ebf3d8d7a8b788e78c4b9a9bf9d73eaad
ASUS <ASUS@192.168.115.145>
2021-12-10 16:39:06 +0100

    séance 2 / 1 (classe joueur)

--- src/tp4_onitama/Joueur.java
+++ src/tp4_onitama/Joueur.java
@@ -4,13 +4,54 @@
  */
 package tp4_onitama;
 
-/**
- *
- * @author thvel
+/*
+String nom	
+String couleur
+Pion [5] pions
+Carte [2] patterns
+Carte jeterUneCarte() (retourne la carte utilisée)
+affecterCouleur(String)
  */
+
 public class Joueur {
     String nom;
     String couleur;
-    Pion [] pions = new Pion [5];
-    // Carte [] patterns                
+    Pion [] pions = new Pion[5];
+    Carte [] patterns = new Carte[2]; 
+    Carte carteUtilisee;
+    
+    public Joueur(String Nom){
+        nom = Nom;
+    }
+    
+    public void affecterCouleur(String Couleur){
+        couleur = Couleur;
+    }
+    
+    public boolean tirerUneCarte(Carte carte){
+        if (patterns[0] != null && patterns[1] != null) {
+            System.out.println("Erreur, vous avez déjà 2 cartes");
+            return false;
+        }
+        if (patterns[0] == null){
+            patterns [0] = carte;
+            return true;
+        }
+        if (patterns[1] == null){
+            patterns [1] = carte;
+            return true;
+        }
+        return false;
+    }
+    
+    public Carte jeterUneCarte(int carte){
+        if ((carte != 1)||(carte != 2)){
+            System.out.println("Erreur, choisis une carte valable");
+        }
+        Carte retour = patterns[carte];
+        patterns[carte] = null;
+        return retour;
+    }
 }
+
+
