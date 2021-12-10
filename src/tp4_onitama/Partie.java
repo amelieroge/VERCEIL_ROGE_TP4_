76cf58eefd363d2cf9b1acafb6b861e6801a0f75
ASUS <ASUS@192.168.115.145>
2021-12-10 19:14:47 +0100

    séance 2 / 5 (classe partie)

--- src/tp4_onitama/Partie.java
+++ src/tp4_onitama/Partie.java
@@ -15,4 +15,64 @@ public class Partie {
     Joueur [] listeJoueurs = new Joueur[2];
     Joueur joueurCourant;
     Grille grilleJeu;
+    Carte [] listeCartes = new Carte[16];
+    
+    public Partie(){
+        int [][] b = {{0,-1},{1,0},{0,1}};
+        Carte cboar = new Carte("Boar", b);
+        listeCartes[0] = cboar;
+
+        int [][] co = {{0,-1},{1,1},{-1,1}};
+        Carte ccobra = new Carte("Cobra", co);
+        listeCartes[1] = ccobra;
+        
+        int [][] crab = {{0,-2},{1,0},{0,2}};
+        Carte ccrab = new Carte("Crab", crab);
+        listeCartes[2] = ccrab;
+        
+        int [][] cran = {{-1,-1},{1,0},{-1,1}};
+        Carte ccrane = new Carte("Boar", cran);
+        listeCartes[3] = ccrane;
+        
+        int [][] d = {{-1,-1},{1,-2},{1,2},{-1,1}};
+        Carte cdragon = new Carte("Dragon", d);
+        listeCartes[4] = cdragon;
+        
+        int [][] ee = {{1,-1},{-1,-1},{0,1}};
+        Carte ceel = new Carte("Eel", ee);
+        listeCartes[5] = ceel;
+        
+        int [][] el = {{0,-1},{1,-1},{0,1},{1,1}};
+        Carte celephant = new Carte("Elephant", el);
+        listeCartes[6] = celephant;
+        
+        int [][] f = {{0,-2},{-1,1},{-1,1}};
+        Carte cfrog = new Carte("Frog", f);
+        listeCartes[7] = cfrog;
+        
+        int [][] g = {{0,-1},{1,-1},{0,1},{-1,1}};
+        Carte cgoose = new Carte("Goose", g);
+        listeCartes[8] = cgoose;
+        
+        int [][] h = {{-1,0},{0,-1},{1,0}};
+        Carte chorse = new Carte("Horse", h);
+        listeCartes[9] = chorse;
+        
+        int [][] ma = {{1,-1},{1,1},{-1,0}};
+        Carte cmantis = new Carte("Mantis", ma);
+        listeCartes[10] = cmantis;
+
+        int [][] mo = {{-1,-1},{-1,1},{1,1},{1,-1}};
+        Carte cmonkey = new Carte("Monkey", mo);
+        listeCartes[11] = cmonkey;
+
+        int [][] o = {{1,0},{0,1},{0,1}};
+        Carte cox = new Carte("Ox", o);
+        listeCartes[12] = cox;
+
+        int [][] ra = {{-1,-1},{1,1},{0,2}};
+        Carte crabbit = new Carte("Rabbit", ra);
+        listeCartes[13] = crabbit;
+
+    }
 }
