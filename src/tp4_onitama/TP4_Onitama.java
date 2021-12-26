de90ceac8219a76502666b207ce7eed3341bc2ac
thvel <thvel@192.168.115.164>
2021-12-26 22:16:25 +0100

    Onitama sceance 3 / BongerPion avec test de victoire à tester

--- src/tp4_onitama/TP4_Onitama.java
+++ src/tp4_onitama/TP4_Onitama.java
@@ -19,12 +19,7 @@ public class TP4_Onitama {
         
         System.out.println("\n");
         
-        int [][] PatternDuDragon = {{1,-1}, { 0,1}};
         
-        //Carte Cartetest = new Carte("Carte du Dragon", PatternDuDragon, );
-    
-        //if (Cartetest.DeplacementCarte(3,3,4,2)) {
-        //System.out.println("Deplacement possible");
     }
         
 }
