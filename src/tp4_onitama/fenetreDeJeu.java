253794fa95b01ce46de4a602f85317a39e7b3510
thvel <thvel@192.168.115.164>
2021-12-27 18:14:26 +0100

    Onitama sceance 4 / partie graphique v0

--- src/tp4_onitama/fenetreDeJeu.java
+++ src/tp4_onitama/fenetreDeJeu.java
@@ -27,19 +27,9 @@ public class fenetreDeJeu extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
-
-        pack();
+        setBounds(0, 0, 1316, 939);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
