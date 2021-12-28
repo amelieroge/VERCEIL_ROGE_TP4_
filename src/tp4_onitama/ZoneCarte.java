08610643a44d5cef6c53ccbf5c591336b7f3ed7c
thvel <thvel@192.168.115.164>
2021-12-28 15:59:36 +0100

    Onitatama graphique seance 28/12 / v18 affichage carte OKKKK

--- src/tp4_onitama/ZoneCarte.java
+++ src/tp4_onitama/ZoneCarte.java
@@ -17,25 +17,7 @@ import javax.swing.ImageIcon;
 public class ZoneCarte extends JButton {
     Carte CarteAssocie;
     
-
     ImageIcon img_carte_vide = new javax.swing.ImageIcon(getClass().getResource("/Images/image_vide.png"));
-    public ImageIcon img_boar = new javax.swing.ImageIcon(getClass().getResource("/Images/boar.jpg"));
-    public ImageIcon img_cobra = new javax.swing.ImageIcon(getClass().getResource("/Images/cobra.jpg"));
-    public ImageIcon img_crab = new javax.swing.ImageIcon(getClass().getResource("/Images/crab.jpg"));
-    public ImageIcon img_crane = new javax.swing.ImageIcon(getClass().getResource("/Images/crane.jpg"));
-    public ImageIcon img_dragon = new javax.swing.ImageIcon(getClass().getResource("/Images/dragon.jpg"));
-    public ImageIcon img_eel = new javax.swing.ImageIcon(getClass().getResource("/Images/eel.jpg"));
-    public ImageIcon img_elephant = new javax.swing.ImageIcon(getClass().getResource("/Images/elephant.jpg"));
-    public ImageIcon img_frog = new javax.swing.ImageIcon(getClass().getResource("/Images/frog.jpg"));
-    public ImageIcon img_goose = new javax.swing.ImageIcon(getClass().getResource("/Images/goose.jpg"));
-    public ImageIcon img_horse = new javax.swing.ImageIcon(getClass().getResource("/Images/horse.jpg"));
-    public ImageIcon img_mantis = new javax.swing.ImageIcon(getClass().getResource("/Images/mantis.jpg"));
-    public ImageIcon img_monkey = new javax.swing.ImageIcon(getClass().getResource("/Images/monkey.jpg"));
-    public ImageIcon img_ox = new javax.swing.ImageIcon(getClass().getResource("/Images/ox.jpg"));
-    public ImageIcon img_rabbit = new javax.swing.ImageIcon(getClass().getResource("/Images/rabbit.jpg"));
-    public ImageIcon img_rooster = new javax.swing.ImageIcon(getClass().getResource("/Images/rooster.jpg"));
-    public ImageIcon img_tiger = new javax.swing.ImageIcon(getClass().getResource("/Images/tiger.jpg"));
-    public ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/Images/image_vide.png"));
 
     // Constructeur
     public ZoneCarte(Carte uneCarte){
@@ -50,59 +32,7 @@ public class ZoneCarte extends JButton {
             setIcon(img_carte_vide);
         }
         else {
-            String img = CarteAssocie.Nom;
-            switch (img) {
-                case "Boar" :
-                    setIcon(img_boar);
-                    break;
-                case "Cobra" :
-                    setIcon(img_cobra);
-                    break;
-                case "Crab" :
-                    setIcon(img_crab);
-                    break;
-                case "Crane" :
-                    setIcon(img_crane);
-                    break;
-                case "Dragon" :
-                    setIcon(img_dragon);
-                    break;
-                case "Eel" :
-                    setIcon(img_eel);
-                    break;
-                case "Elephant" :
-                    setIcon(img_elephant);
-                    break;
-                case "Frog" :
-                    setIcon(img_frog);
-                    break;    
-                case "Goose" :
-                    setIcon(img_goose);
-                    break;    
-                case "Horse" :
-                    setIcon(img_horse);
-                    break;    
-                case "Mantis" :
-                    setIcon(img_mantis);
-                    break;    
-                case "Monkey" :
-                    setIcon(img_monkey);
-                    break;   
-                case "Ox" :
-                    setIcon(img_ox);
-                    break; 
-                case "Rabbit" :
-                    setIcon(img_rabbit);
-                    break;
-                case "Rooster" :
-                    setIcon(img_rooster);
-                    break;
-                case "Tiger" :
-                    setIcon(img_tiger);
-                    break;
-            }
-
-
+            setIcon(CarteAssocie.Image);
         }
         
     }
