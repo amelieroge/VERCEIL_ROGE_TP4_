68d58ed0e3751ebb923064c0b5d96a4b47c0338c
ASUS <ASUS@192.168.115.145>
2021-12-28 14:32:24 +0100

    seance 8 / 5 tentatice 1

--- src/tp4_onitama/ZoneCarte.java
+++ src/tp4_onitama/ZoneCarte.java
@@ -20,6 +20,24 @@ public class ZoneCarte extends JButton {
     
     ImageIcon img_carte_vide = new javax.swing.ImageIcon(getClass().getResource("/Images/image_vide.png"));
     
+    public ImageIcon img_boar = new javax.swing.ImageIcon(getClass().getResource("/Images/boar.jpg"));
+    public ImageIcon img_cobra = new javax.swing.ImageIcon(getClass().getResource("/Images/cobra.jpg"));
+    public ImageIcon img_crab = new javax.swing.ImageIcon(getClass().getResource("/Images/crab.jpg"));
+    public ImageIcon img_crane = new javax.swing.ImageIcon(getClass().getResource("/Images/crane.jpg"));
+    public ImageIcon img_dragon = new javax.swing.ImageIcon(getClass().getResource("/Images/dragon.jpg"));
+    public ImageIcon img_eel = new javax.swing.ImageIcon(getClass().getResource("/Images/eel.jpg"));
+    public ImageIcon img_elephant = new javax.swing.ImageIcon(getClass().getResource("/Images/elephant.jpg"));
+    public ImageIcon img_frog = new javax.swing.ImageIcon(getClass().getResource("/Images/frog.jpg"));
+    public ImageIcon img_goose = new javax.swing.ImageIcon(getClass().getResource("/Images/goose.jpg"));
+    public ImageIcon img_horse = new javax.swing.ImageIcon(getClass().getResource("/Images/horse.jpg"));
+    public ImageIcon img_mantis = new javax.swing.ImageIcon(getClass().getResource("/Images/mantis.jpg"));
+    public ImageIcon img_monkey = new javax.swing.ImageIcon(getClass().getResource("/Images/monkey.jpg"));
+    public ImageIcon img_ox = new javax.swing.ImageIcon(getClass().getResource("/Images/ox.jpg"));
+    public ImageIcon img_rabbit = new javax.swing.ImageIcon(getClass().getResource("/Images/rabbit.jpg"));
+    public ImageIcon img_rooster = new javax.swing.ImageIcon(getClass().getResource("/Images/rooster.jpg"));
+    public ImageIcon img_tiger = new javax.swing.ImageIcon(getClass().getResource("/Images/tiger.jpg"));
+    public ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/Images/image_vide.png"));
+    
     // Constructeur
     public ZoneCarte(Carte uneCarte){
         CarteAssocie = uneCarte;
@@ -33,7 +51,59 @@ public class ZoneCarte extends JButton {
             setIcon(img_carte_vide);
         }
         else {
-          setIcon(CarteAssocie.Image);  
+            String img = CarteAssocie.Image.toString();
+            switch (img) {
+                case "img_boar" :
+                    setIcon(img_boar);
+                    break;
+                case "img_cobra" :
+                    setIcon(img_cobra);
+                    break;
+                case "img_crab" :
+                    setIcon(img_crab);
+                    break;
+                case "img_crane" :
+                    setIcon(img_crane);
+                    break;
+                case "img_dragon" :
+                    setIcon(img_dragon);
+                    break;
+                case "img_eel" :
+                    setIcon(img_eel);
+                    break;
+                case "img_elephant" :
+                    setIcon(img_elephant);
+                    break;
+                case "img_frog" :
+                    setIcon(img_frog);
+                    break;    
+                case "img_goose" :
+                    setIcon(img_goose);
+                    break;    
+                case "img_horse" :
+                    setIcon(img_horse);
+                    break;    
+                case "img_mantis" :
+                    setIcon(img_mantis);
+                    break;    
+                case "img_monkey" :
+                    setIcon(img_monkey);
+                    break;   
+                case "img_ox" :
+                    setIcon(img_ox);
+                    break; 
+                case "img_rabbit" :
+                    setIcon(img_rabbit);
+                    break;
+                case "img_rooster" :
+                    setIcon(img_rooster);
+                    break;
+                case "img_tiger" :
+                    setIcon(img_tiger);
+                    break;
+            }
+
+
         }
         
     }
